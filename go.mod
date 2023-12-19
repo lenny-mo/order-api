6286e9be1a5fa7cd8e9b11fb6fe308d11325b963
Lenny Mo <lennymo.work@gmail.com>
2023-12-19 21:42:56 +0800

    add jaeger

--- go.mod
+++ go.mod
@@ -8,11 +8,13 @@ replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
 
 require (
 	github.com/golang/protobuf v1.5.3
+	github.com/lenny-mo/emall-utils v0.0.0-20231218141407-3b3960e96cd9
 	github.com/lenny-mo/order v0.0.0-20231217134738-75be0c9de598
 	github.com/micro/go-micro/v2 v2.9.1
 	github.com/micro/go-plugins/registry/consul/v2 v2.9.1
+	github.com/micro/go-plugins/wrapper/trace/opentracing/v2 v2.9.1
 	github.com/miekg/dns v1.1.57 // indirect
-	golang.org/x/net v0.19.0 // indirect
+	github.com/opentracing/opentracing-go v1.2.0
 	golang.org/x/tools v0.16.1 // indirect
 	google.golang.org/protobuf v1.31.0
 )
--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 
+	"github.com/lenny-mo/emall-utils/tracer"
 	"github.com/lenny-mo/order-api/handler"
 	"github.com/lenny-mo/order-api/proto/orderapi"
 	"github.com/lenny-mo/order/proto/order"
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-plugins/registry/consul/v2"
+	opentracing2 "github.com/micro/go-plugins/wrapper/trace/opentracing/v2"
+	"github.com/opentracing/opentracing-go"
 )
 
 func main() {
@@ -19,18 +22,30 @@ func main() {
 		}
 	})
 
+	serviceName := "go.micro.api.order-api"
+
+	// 开启链路追踪
+	err := tracer.InitTracer(serviceName, "127.0.0.1:6831")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tracer.Closer.Close()
+	opentracing.SetGlobalTracer(tracer.Tracer)
+
 	// 创建服务
 	service := micro.NewService(
-		micro.Name("go.micro.api.order-api"),
+		micro.Name(serviceName),
 		micro.Version("latest"),
 		micro.Address("127.0.0.1:8090"),
 		micro.Registry(consulRegistry),
+		// 添加链路追踪
+		micro.WrapClient(opentracing2.NewClientWrapper(opentracing.GlobalTracer())),
 	)
-
 	service.Init()
 
 	orderService := order.NewOrderService("go.micro.service.order", service.Client())
-	err := orderapi.RegisterOrderApiHandler(service.Server(), &handler.OrderApi{
+	err = orderapi.RegisterOrderApiHandler(service.Server(), &handler.OrderApi{
 		OrderService: orderService,
 	})
 	if err != nil {
@@ -38,6 +53,7 @@ func main() {
 		return
 	}
 
+	tracer.InitTracer("orderapi", "127.0.0.1:6831")
 	// 启动服务
 	if err := service.Run(); err != nil {
 		fmt.Println(err)
